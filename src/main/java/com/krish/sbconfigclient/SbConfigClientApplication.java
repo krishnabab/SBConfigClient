95022b85dade4b26d18ea349ad600aed5aa43c1e
Krishna babu Ghanta <krishna@Krishnas-MacBook-Pro.local>
2020-12-05 14:16:03 -0600

    first commit

---
+++ src/main/java/com/krish/sbconfigclient/SbConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.krish.sbconfigclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbConfigClientApplication.class, args);
+    }
+
+}
